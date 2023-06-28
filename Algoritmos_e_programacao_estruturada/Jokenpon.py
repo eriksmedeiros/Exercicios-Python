a199ca6904d7b1229929fa8dbdc95c51b7b0bae2
Erik Medeiros <erik.medeiros.701@ufrn.edu.br>
2023-06-27 23:26:26 -0300

    jokenpon finalizado

--- Algoritmos_e_programacao_estruturada/Jokenpon.py
+++ Algoritmos_e_programacao_estruturada/Jokenpon.py
@@ -1,32 +1,66 @@
+
 import random
 
 jogadas = ['Pedra', 'Papel', 'Tesoura']
 
 modo = int(input('Digite \n1 para Rodada Única \n2 para Melhor de 5\n'))
 
+# Rodada única
 if modo == 1:
     computador = random.choice(jogadas)
     usuario = input('Pedra, Papel ou Tesoura? ')
     print(computador)
     print(usuario)
     
+    if usuario == computador:
+        print('empate')
 
-    if usuario == 'Papel' and computador == 'Papel':
-        print('empate')
-    elif usuario == 'Pedra' and computador == 'Pedra':
-        print('empate')
-    elif usuario == 'Tesoura' and computador == 'Tesoura':
-        print('empate')
+
     elif usuario == 'Papel' and computador == 'Tesoura':
         print('Computador winner')
     elif usuario == 'Papel' and computador == 'Pedra':
         print('Usuário Winner')
+
     elif usuario == 'Pedra' and computador == 'Tesoura':
         print('Usuário Winner')
     elif usuario == 'Pedra' and computador == 'Papel':
         print('Computador Winner')
+
     elif usuario == 'Tesoura' and computador == 'Pedra':
         print('Computador Winner')
     elif usuario == 'Tesoura' and computador == 'Papel':
         print('Usuário Winner')
 
+    input("Pressione Enter para sair...")
+
+# Melhor de 5
+if modo == 2:
+
+    usuario = 0
+
+    for usuario in range(5):
+
+        usuario = input('Pedra, Papel ou Tesoura? ')
+
+        computador = random.choice(jogadas)
+
+        if usuario == computador:
+            print('empate')
+
+
+        elif usuario == 'Papel' and computador == 'Tesoura':
+            print('Computador winner')
+        elif usuario == 'Papel' and computador == 'Pedra':
+            print('Usuário Winner')
+
+        elif usuario == 'Pedra' and computador == 'Tesoura':
+            print('Usuário Winner')
+        elif usuario == 'Pedra' and computador == 'Papel':
+            print('Computador Winner')
+
+        elif usuario == 'Tesoura' and computador == 'Pedra':
+            print('Computador Winner')
+        elif usuario == 'Tesoura' and computador == 'Papel':
+            print('Usuário Winner')
+
+    input("Pressione Enter para sair...")
