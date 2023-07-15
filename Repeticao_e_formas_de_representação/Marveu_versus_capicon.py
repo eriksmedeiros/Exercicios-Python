36865c2aa95e9fbf2691bea5a84c183bca8c6d8d
Erik Medeiros <erik.medeiros.701@ufrn.edu.br>
2023-07-14 22:29:40 -0300

    exercicio 2 completo

--- Repeticao_e_formas_de_representação/Marveu_versus_capicon.py
+++ Repeticao_e_formas_de_representação/Marveu_versus_capicon.py
@@ -4,8 +4,80 @@ def encontre_mínimo(lista):
 def encontre_máximo(lista):
     return max(numeros)
 
-def momento_mínimo_informado():
+def momento_mínimo_informado(lista):
     for indice, valor in enumerate(numeros):
         indice += 1
-        if marveu == min(numeros):
-            print('você acertou')
+        if valor == min(numeros):
+            return indice
+        
+def momento_máximo_informado(lista):
+    for indice, valor in enumerate(numeros):
+        indice += 1
+        if valor == max(numeros):
+            return indice
+        
+def somatório(lista):
+    return sum(numeros)
+
+def produtório(lista):
+  produto = 1  
+  for i in numeros:  
+    produto *= i
+  return produto
+
+def média(lista):
+    return sum(numeros)/10
+
+def mediana(lista):
+    numeros.sort()
+    med = []
+    for indice, valor in enumerate(numeros):
+        indice += 1
+        if indice == 5:
+            med.append(valor)
+        if indice == 6:
+            med.append(valor)
+    return sum(med)/2
+
+def numeros_primos(numero):
+  primos = []
+  for i in range(numero+1):
+    i += 1
+    if numero%i == 0:
+        primos.append(i)
+  if primos == [1, numero]:
+    return True
+  return False
+
+def todos_primos(lista):
+  for i in numeros:
+    if not numeros_primos(i):
+      return False
+  return True
+
+def algum_primo(lista):
+  for i in numeros:
+    if numeros_primos(i):
+      return True
+  return False
+
+numeros = []
+
+for i in range(10):
+   num = int(input('Digite um número: '))
+   numeros.append(num)
+
+print('Mínimo:', encontre_mínimo(numeros))
+print('Máximo:', encontre_máximo(numeros))
+
+print('Momento do mínimo:', momento_mínimo_informado(numeros))
+print('Momento do máximo:', momento_máximo_informado(numeros))
+
+print('Somatório:', somatório(numeros))
+print('Produtório:', produtório(numeros))
+
+print('Média:', média(numeros))
+print('Mediana:', mediana(numeros))
+
+print('Todos os números são primos:', todos_primos(numeros))
+print('Tem algum número primo:', algum_primo(numeros))
